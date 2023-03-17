69956561585c38796241e195f1959e30c51a4ac5
Bill Sideris <bill88t@feline.gr>
2023-03-17 16:30:19 +0200

    put is almost done

--- cptoml.py
+++ cptoml.py
@@ -6,7 +6,7 @@ def _rcm(line) -> str:
     Remove comments from a line buffer.
     Also removes misc chars.
     """
-    if line.rfind("#") != -1: # Without the check -1 eats a char
+    if line.rfind("#") != -1:  # Without the check -1 eats a char
         line = line[: line.rfind("#")]
     while line.endswith(" ") or line.endswith("\n"):
         line = line[:-1]
@@ -32,25 +32,25 @@ def _ef(line):
     """
     result = _rcm(line)
     result = result[result.find("=") + 1 :]
-    result = _rcm(result) # for spaces
+    result = _rcm(result)  # for spaces
 
     if (result.startswith('"') and result.endswith('"')) or (
         result.startswith("'") and result.endswith("'")
-    ): # chars
+    ):  # chars
         result = result[1:-1]
-    elif result.isdigit() or (result[0] in ["-", "+"] and result[1:].isdigit()): # ints
+    elif result.isdigit() or (result[0] in ["-", "+"] and result[1:].isdigit()):  # ints
         result = int(result)
-    elif result[0] == "0" and result[1].isletter(): # other numeric
-        if result[1] == "x": # hex
+    elif result[0] == "0" and result[1].isletter():  # other numeric
+        if result[1] == "x":  # hex
             result = hex(int(result[2:], 16))
-        elif result[1] == "o": # octal
+        elif result[1] == "o":  # octal
             result = oct(int(result[2:], 8))
-        elif result[0].isdigit() and ("e" in result): # notation
+        elif result[0].isdigit() and ("e" in result):  # notation
             exec(f"result = int({result})")
         else:
             del line, result
             raise TypeError("Invalid value.")
-    elif result == "true": # bools
+    elif result == "true":  # bools
         result = True
     elif result == "false":
         result = False
@@ -88,6 +88,32 @@ def _lf(buf, key, start=0) -> int:
     del buf, start, key, tm, q
     return result
 
+
+def _lm(key, value, comment=None) -> str:
+    """
+    Creates a new toml line with key and value.
+    Accepts comment.
+    """
+    result = key + " = "
+    if isinstance(value, str):
+        result += '"' + value.replace("\\", "\\\\") + '"'  # make raw
+    elif isinstance(value, int) or isinstance(value, float):
+        if str(value) != "inf":
+            result += str(value)
+        else:
+            del result, key, value, comment
+            raise TypeError("Value infinite")
+    elif isinstance(value, bool):
+        result += str(value).lower()
+    else:
+        del result, key, value, comment
+        raise TypeError("Unsupported type for toml")
+    if comment is not None:
+        result += " # " + comment
+    del key, value, comment
+    return result
+
+
 def _tf(buf, subtable) -> int:
     """
     Find subtable buffer index.
@@ -98,6 +124,7 @@ def _tf(buf, subtable) -> int:
     tm = 0
     q = True
     while q:
+        tml = None
         try:
             tml = _rcm(buf[tm])
         except IndexError:
@@ -112,6 +139,7 @@ def _tf(buf, subtable) -> int:
     del buf, subtable, tm, q
     return result
 
+
 def fetch(item, subtable=None, toml="/settings.toml"):
     if not isinstance(item, str):
         del item, subtable, toml
@@ -121,20 +149,20 @@ def fetch(item, subtable=None, toml="/settings.toml"):
         raise TypeError("Subtable should be str.")
     try:
         with open(toml) as tomlf:
-            data = _df(tomlf.read()) # load into list
+            data = _df(tomlf.read())  # load into list
             result = None
             if subtable is None:  # Browse root table
                 target = _lf(data, item)
                 if target != -1:
                     result = _ef(data[target])
             else:
-                start = _tf(data, subtable) # find table offset
+                start = _tf(data, subtable)  # find table offset
                 if start != -1:
                     start += 1
-                if start != -1: # table found
-                    tr = _lf(data, item, start) # fetch item index
+                if start != -1:  # table found
+                    tr = _lf(data, item, start)  # fetch item index
                     if tr != -1:
-                        result = _ef(data[tr]) # load value
+                        result = _ef(data[tr])  # load value
                     del tr
                 del start
             del data, subtable, toml, item
@@ -162,17 +190,17 @@ def put(item, value, subtable=None, toml="/settings.toml", comment=None) -> None
         # Find target line
         start = 0
         if subtable is not None:
-            start = _tf(data, subtable) # find table offset
-            del start
-
-        if subtable == -1: # Need to create new subtable
-            pass
-        else: # Whatever start says
-            tr = _lf(data, item, start) # fetch item index
-            if tr == -1: # Insert
-                pass
-            else: # Update
-                pass
+            start = _tf(data, subtable)  # find table offset
+        if start == -1:  # Need to create new subtable
+            data.append(f"[{subtable}]")
+            data.append(_lm(item, value, comment))
+        else:  # Whatever start says
+            tr = _lf(data, item, start)  # fetch item index
+            if tr == -1:  # New key
+                data.insert(start + 1, _lm(item, value, comment))
+            else:  # Existing key
+                data[tr] = _lm(item, value, comment)
+        del start
 
         # Reapply formatting
         for i in range(len(data) - 2, 0, -1):
@@ -183,19 +211,20 @@ def put(item, value, subtable=None, toml="/settings.toml", comment=None) -> None
         print(str(data))
 
         # Write to file
-        if getmount("/").readonly: # If it wasn't we won't make it
-            ro = True
-            print("Remounted")
-            remount("/", False)
+        # if getmount("/").readonly:  # If it wasn't we won't make it
+        #    ro = True
+        #    print("Remounted")
+        #    remount("/", False)
         # with open(toml, "w") as tomlw:
         #    for line in data:
         #        tomlw.write(f"{line}\n")
         #        del line
-        if ro:
-            remount("/", True)
+        # if ro:
+        #    remount("/", True)
 
     del item, value, subtable, toml, comment, data, ro
 
 
 def delete(item, subtable=None, toml="/settings.toml") -> None:
-    del item, subtable, toml
+    raise NotImplementedError
+    # del item, subtable, toml
